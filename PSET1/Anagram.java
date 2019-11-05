02ae3a1051fb02befd463f7f05530269a762307f
tengfone <20770447+tengfone@users.noreply.github.com>
2019-11-05 18:29:44 +0800

    Update Anagram.java

--- PSET1/Anagram.java
+++ PSET1/Anagram.java
@@ -22,3 +22,28 @@ public class MyClass {
         return true;
     }
 }
+
+// method 2
+    public static boolean isAnagram(String a, String b) {
+        a = a.toUpperCase();
+        b = b.toUpperCase();
+        boolean output = false;
+        StringBuilder c = new StringBuilder(b);
+
+        if (a.length() == b.length()) {
+            for (int i = 0; i < a.length(); i++) {
+                for (int j = 0; j < c.length(); j++) {
+                    if (a.charAt(i) == c.charAt(j)) {
+                        c.deleteCharAt(j);
+                        if (i == a.length() - 1 && c.length() == 0) {
+                            output = true;
+                            break;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return output;
+
+    }
